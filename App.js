20bc4363b1d01c56209b98048716b21de932d7c7
torigoe0606 <108105733+torigoe0606@users.noreply.github.com>
2022-09-22 15:38:12 +0900

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <header><h1>ログイン画面</h1></header>
+      <p>メールアドレス</p>
+      <input type="1text" value="01234@webfronier.ac.jp"></input>
+      <p>パスワード</p>
+      <input type="2text" value="****"></input>
+      <p></p>
+
+      <a href="professor.js" class="btn">ログイン</a>
+
+    </div>
+  );
+}
+
+export default App;
